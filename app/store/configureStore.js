1eed5107f3994039a6cafed3e7c537799ab40fcd
Øyvind Nordsveen <oyvind.nordsveen@sol.no>
2016-03-17 12:41:05 +0100

    Removed old code

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -31,22 +31,6 @@ const logger = createLogger({
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(sagaMiddleware(...sagas), logger)
-
-    //DevTools.instrument(),
-    //persistState(
-    //  window.location.href.match(
-    //    /[?&]debug_session=([^&]+)\b/
-      //)
-    //)
   ));
-
-  //if (module.hot) {
-  //   Enable Webpack hot module replacement for reducers
-    //module.hot.accept('../reducers', () => {
-    //  const nextReducer = require('../reducers');
-    //  store.replaceReducer(nextReducer);
-    //});
-  //}
-
   return store;
 }
