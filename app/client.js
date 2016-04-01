fd2f7efcf669f37379de16aa80b66d7c8087542e
Øyvind Nordsveen <oyvind.nordsveen@sol.no>
2016-04-01 12:47:25 +0200

    refactoring

--- app/client.js
+++ app/client.js
@@ -9,9 +9,7 @@ import {simulateWorld} from './game/simulateWorld';
 import {setupKeyBindings}from'./setupKeyBindings';
 import {createMap} from './game/createMap';
 import {setWorld} from './actions/index';
-import {selectEntities} from './game/mapUtils';
-import {getTool} from './game/tools';
-import {setSelectedEntities} from './actions';
+import {doInputHandling} from './inputHandling';
 import './app.global.css';
 
 const store = configureStore();
@@ -29,37 +27,7 @@ render(
 );
 
 
-function doInputHandling() {
-  let state = store.getState();
-  if (state.input.dragEvent) {
-    let tool = getTool(state.input.selectedTool);
-    let xStart = state.input.mouseStartDragXPos;
-    let yStart = state.input.mouseStartDragYPos;
-    let xEnd = state.input.mouseXPos;
-    let yEnd = state.input.mouseYPos;
-    let width = xEnd - xStart;
-    let height = yEnd - yStart;
 
-    //make sure start point is the smallest coordinate
-    if(xStart > xEnd) {
-      xStart = xEnd;
-    }
-    if(yStart < yEnd){
-      yStart = yEnd
-    }
-
-    width = width > 0 ? width : width * -1;
-    height = height > 0 ? height : height * -1;
-    width = Math.ceil(width / 16);
-    height = Math.ceil(height / 16);
-    let startXTile = Math.floor(xStart / 16);
-    let startYTile = Math.floor((512 - yStart) / 16);
-    
-    let selected = selectEntities(state.world.map, tool.affectTag, startXTile,startYTile,width, height);
-    store.dispatch(setSelectedEntities(selected)); 
-    store.dispatch({type: 'MOUSE_DRAG_DONE'});
-  }
-}
 
 function renderWorld() {
   drawWorld(store.getState());
@@ -74,7 +42,7 @@ function doSimulationStep() {
 setInterval(doSimulationStep, 1000);
 
 function renderMouse() {
-  doInputHandling();
+  doInputHandling(store);
   drawMouse(store.getState());
   requestAnimationFrame(renderMouse);
 }
